1d137cf80efc36925a4ed7e40887578cdabc0e57
Jonathan Kleffel <jklef24@student.sdu.dk>
2025-09-27 14:51:29 +0200

    Created sample post with icons.

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -8,8 +8,14 @@
     {{-- main --}}
     <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
         <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
-            
+            {{-- Posts --}}
+            <section class="mt-5 space-y-4 p-2">
+                <livewire:post.item />
+            </section>
+        
         </aside>
+        
+        
 
         <aside class="lg:col-span-4 border hidden lg:block p-4">
             
--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -1,3 +1,36 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
+<div class="max-w-lg mx-auto">
+    {{-- header --}}
+    <header class="flex items-center gap-3 mb-2">
+        <x-avatar class="h-12 w-12" />
+
+        <div class="flex justify-between items-center w-full">
+            <h5 class="font-semibold text-sm">{{ fake()->name }}</h5>
+            <button>
+                <x-tabler-dots />
+            </button>
+        </div>
+    </header>
+
+    {{-- main --}}
+    <main class="my-4">
+        <img
+            src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+            alt="Post image"
+            class="w-full rounded-lg object-cover" />
+    </main>
+
+    {{-- footer --}}
+    <footer>
+        <div class="flex gap-4 items-center my-2">
+            <span>
+                <x-tabler-thumb-up />
+            </span>
+            <span>
+                <x-tabler-message-circle-2 />
+            </span>
+            <span class="ml-auto">
+                <x-tabler-share-3 />
+            </span>
+        </div>
+    </footer>
 </div>
