bd7d2d2ff2d7913be77fd7d0a9450773d8b46802
Jonathan Kleffel <jklef24@student.sdu.dk>
2025-10-06 19:45:06 +0200

    Added register link in the login page and improved appearance.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,14 +39,17 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
+        <div class="flex items-center justify-center mt-4">
+            <x-primary-button class="w-full max-w-md flex justify-center items-center">
                 {{ __('Register') }}
             </x-primary-button>
+        </div>
+
+        <div class="flex items-center justify-center mt-4">
+            <a class="text-sm text-gray-600 dark:text-gray-400 hover:underline hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
         </div>
     </form>
 </x-guest-layout>
