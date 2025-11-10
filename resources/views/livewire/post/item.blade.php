3b0f445ff2f6344747e68131ed6728c8a1359927
Jonathan Kleffel <jklef24@student.sdu.dk>
2025-11-10 20:11:38 +0100

    Prevent like and comment alter as a guest.

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -80,11 +80,12 @@
     {{-- Footer --}}
     <footer>
         <div class="flex gap-4 items-center my-2">
+            @auth
             <button
                 class="like-btn"
                 data-post-id="{{ $post->id }}"
-                data-liked="{{ auth()->check() && $post->isLikedBy(auth()->user()) ? 'true' : 'false' }}">
-                @if(auth()->check() && $post->isLikedBy(auth()->user()))
+                data-liked="{{ $post->isLikedBy(auth()->user()) ? 'true' : 'false' }}">
+                @if($post->isLikedBy(auth()->user()))
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-thumb-up">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M13 3a3 3 0 0 1 2.995 2.824l.005 .176v4h2a3 3 0 0 1 2.98 2.65l.015 .174l.005 .176l-.02 .196l-1.006 5.032c-.381 1.626 -1.502 2.796 -2.81 2.78l-.164 -.008h-8a1 1 0 0 1 -.993 -.883l-.007 -.117l.001 -9.536a1 1 0 0 1 .5 -.865a2.998 2.998 0 0 0 1.492 -2.397l.007 -.202v-1a3 3 0 0 1 3 -3z" />
@@ -97,10 +98,25 @@
                 </svg>
                 @endif
             </button>
+            @else
+            <button class="like-btn" disabled title="Login to like posts">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-thumb-up">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3" />
+                </svg>
+            </button>
+            @endauth
             <span id="like-count-{{ $post->id }}">{{ $post->likes_count ?? 0 }}</span>
+
+            @auth
             <button onclick="window.location='{{ route('post.show', $post) }}'">
                 <x-tabler-message-circle-2 />
             </button>
+            @else
+            <button disabled title="Login to comment">
+                <x-tabler-message-circle-2 />
+            </button>
+            @endauth
             <span id="comment-count-{{ $post->id }}">{{ $post->comments_count ?? 0 }}</span>
         </div>
     </footer>
