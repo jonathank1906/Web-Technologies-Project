9a2f9b0f626f0efeee063416f553129b908145cf
Flamezam <muazz24@student.sdu.dk>
2025-11-10 07:18:33 +0100

    first part

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,65 +9,80 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts & Other Imports -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" ... />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-8kH2cSWpqVZgUAljS3RhoQ6aE8SR5GqAc4D8RQE3zTXdyxQn8uEr27+PuOi6H0pb+zclZP1ZPH3u9d7D5eY4zg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Scripts & Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     @livewireStyles
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen">
-        <!-- Custom Sidebar -->
-        <nav class="bg-base-200 border-t sm:border-t-0 sm:border-r border-base-300  z-50" id="sidebar">
+    <div class="min-h-screen flex">
+
+        <!-- Sidebar -->
+        <nav class="bg-base-200 border-t sm:border-t-0 sm:border-r border-base-300 z-50 w-60" id="sidebar">
             <ul class="sidebar-menu flex flex-col h-full">
+
+                {{-- Always Visible Links (Guests + Auth Users) --}}
                 <li class="sidebar-item">
-                    <a href="/" class="sidebar-link flex items-center hover:bg-base-300">
+                    <a href="{{ route('home') }}" class="sidebar-link flex items-center hover:bg-base-300 p-3">
                         <x-monoicon-home class="sidebar-icon text-base-content" />
-                        <span class="sidebar-text">Home</span>
+                        <span class="sidebar-text ml-3">Home</span>
                     </a>
                 </li>
+
                 <li class="sidebar-item">
-                    <a href="{{ route('connections') }}" class="sidebar-link hover:bg-base-300">
+                    <a href="{{ route('connections') }}" class="sidebar-link flex items-center hover:bg-base-300 p-3">
                         <x-tabler-wifi class="sidebar-icon text-base-content" />
-                        <span class="sidebar-text">Connections</span>
+                        <span class="sidebar-text ml-3">Connections</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a href="{{ route('messages') }}" class="sidebar-link hover:bg-base-300">
-                        <x-tabler-messages class="sidebar-icon text-base-content" />
-                        <span class="sidebar-text">Messages</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="{{ route('swipe') }}" class="sidebar-link hover:bg-base-300">
-                        <x-tabler-heart class="sidebar-icon" />
-                        <span class="sidebar-text">Swiper</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="{{ route('profile.my') }}" class="sidebar-link hover:bg-base-300">
-                        <x-css-profile class="sidebar-icon" />
-                        <span class="sidebar-text">Profile</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="{{ route('post.create') }}" class="sidebar-link hover:bg-base-300">
-                        <x-tabler-plus class="sidebar-icon" />
-                        <span class="sidebar-text">Create Post</span>
-                    </a>
-                </li>
+
+                {{-- Authenticated-Only Links --}}
+                @auth
+                    <li class="sidebar-item">
+                        <a href="{{ route('messages') }}" class="sidebar-link flex items-center hover:bg-base-300 p-3">
+                            <x-tabler-messages class="sidebar-icon text-base-content" />
+                            <span class="sidebar-text ml-3">Messages</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item">
+                        <a href="{{ route('swipe') }}" class="sidebar-link flex items-center hover:bg-base-300 p-3">
+                            <x-tabler-heart class="sidebar-icon text-base-content" />
+                            <span class="sidebar-text ml-3">Swiper</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item">
+                        <a href="{{ route('profile.my') }}" class="sidebar-link flex items-center hover:bg-base-300 p-3">
+                            <x-css-profile class="sidebar-icon text-base-content" />
+                            <span class="sidebar-text ml-3">Profile</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item">
+                        <a href="{{ route('post.create') }}" class="sidebar-link flex items-center hover:bg-base-300 p-3">
+                            <x-tabler-plus class="sidebar-icon text-base-content" />
+                            <span class="sidebar-text ml-3">Create Post</span>
+                        </a>
+                    </li>
+                @endauth
+
+                {{-- You can add a footer or extra nav below --}}
                 <x-more-nav />
             </ul>
         </nav>
 
         <!-- Main Content -->
-        <main class="bg-base-100" id="mainContent">
+        <main class="flex-1 bg-base-100" id="mainContent">
             {{ $slot }}
         </main>
     </div>
+
     @livewireScripts
 </body>
-
 </html>
